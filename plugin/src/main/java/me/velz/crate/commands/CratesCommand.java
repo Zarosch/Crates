d88f235d963dcb0cbab139cf5cb398dfe82d4a69
Zarosch <kevin.velz@gmail.com>
2019-04-27 21:11:53 +0200

    :bug: Fix Nullpointerexception by giving wrong amount of arguments

--- plugin/src/main/java/me/velz/crate/commands/CratesCommand.java
+++ plugin/src/main/java/me/velz/crate/commands/CratesCommand.java
@@ -10,22 +10,22 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 
 public class CratesCommand implements CommandExecutor {
-    
+
     private final Crates plugin;
 
     public CratesCommand(Crates plugin) {
         this.plugin = plugin;
     }
-    
+
     @Override
     public boolean onCommand(CommandSender cs, Command cmd, String label, String[] args) {
-        
-        if(!cs.hasPermission("crates.command")) {
+
+        if (!cs.hasPermission("crates.command")) {
             cs.sendMessage(MessageUtil.PREFIX.getLocal() + MessageUtil.ERROR_NOPERMISSIONS.getLocal());
             return true;
         }
-        
-        if(args.length == 0) {
+
+        if (args.length == 0) {
             cs.sendMessage("");
             cs.sendMessage(MessageUtil.COMMAND_HELP_RELOAD.getLocal());
             cs.sendMessage(MessageUtil.COMMAND_HELP_GIVE.getLocal());
@@ -33,72 +33,75 @@ public class CratesCommand implements CommandExecutor {
             cs.sendMessage("");
             return true;
         }
-        
-        if(args[0].equalsIgnoreCase("reload")) {
+
+        if (args[0].equalsIgnoreCase("reload")) {
             plugin.getFileManager().load();
             cs.sendMessage(MessageUtil.PREFIX.getLocal() + MessageUtil.COMMAND_RELOADED.getLocal());
             return true;
         }
-        
-        if(args[0].equalsIgnoreCase("additem")) {
-            if(args.length != 2) {
-                cs.sendMessage(MessageUtil.PREFIX.getLocal() + MessageUtil.ERROR_SYNTAX.getLocal());
+
+        if (args[0].equalsIgnoreCase("additem")) {
+            if (args.length != 2) {
+                cs.sendMessage(MessageUtil.PREFIX.getLocal() + MessageUtil.ERROR_SYNTAX.getLocal().replaceAll("%command", "/crate additem [crate]"));
                 return true;
             }
-            if(!(cs instanceof Player)) {
+            if (!(cs instanceof Player)) {
                 cs.sendMessage(MessageUtil.PREFIX.getLocal() + MessageUtil.ERROR_PLAYERONLY.getLocal());
                 return true;
             }
-            
-            Player player = (Player)cs;
+
+            Player player = (Player) cs;
             String crate = args[1];
-            
-            if(!plugin.getCrates().containsKey(crate)) {
+
+            if (!plugin.getCrates().containsKey(crate)) {
                 cs.sendMessage(MessageUtil.PREFIX.getLocal() + MessageUtil.ERROR_CRATENOTFOUND.getLocal());
                 return true;
             }
-            
+
             Inventory inventory = Bukkit.createInventory(null, 27, "Crate: " + crate);
             player.openInventory(inventory);
             cs.sendMessage(MessageUtil.PREFIX.getLocal() + MessageUtil.COMMAND_ADDITEM_OPEN.getLocal());
             return true;
         }
-        
-        if(args[0].equalsIgnoreCase("give")) {
-            Player player = (Player)cs;
-            
-            if(args.length == 3) {
-                if(Bukkit.getPlayer(args[2]) == null) {
+
+        if (args[0].equalsIgnoreCase("give")) {
+            Player player = (Player) cs;
+
+            if (args.length != 3 && args.length != 2) {
+                cs.sendMessage(MessageUtil.PREFIX.getLocal() + MessageUtil.ERROR_SYNTAX.getLocal().replaceAll("%command", "/crate give [crate] [player]"));
+                return true;
+            }
+
+            if (args.length == 3) {
+                if (Bukkit.getPlayer(args[2]) == null) {
                     cs.sendMessage(MessageUtil.PREFIX.getLocal() + MessageUtil.ERROR_PLAYERNOTFOUND.getLocal());
                     return true;
                 }
                 player = Bukkit.getPlayer(args[2]);
             }
-            
+
             String crate = args[1];
-            if(!plugin.getCrates().containsKey(crate)) {
+            if (!plugin.getCrates().containsKey(crate)) {
                 cs.sendMessage(MessageUtil.PREFIX.getLocal() + MessageUtil.ERROR_CRATENOTFOUND.getLocal());
                 return true;
             }
-            
+
             player.getInventory().addItem(plugin.getCrates().get(crate).getItem());
             cs.sendMessage(MessageUtil.PREFIX.getLocal() + MessageUtil.COMMAND_GIVE.getLocal().replaceAll("%player", player.getName()).replaceAll("%crate", crate));
             return true;
         }
-        
-        if(args[0].equalsIgnoreCase("list")) {
+
+        if (args[0].equalsIgnoreCase("list")) {
             String crates = "";
-            for(String crate : plugin.getCrates().keySet()) {
+            for (String crate : plugin.getCrates().keySet()) {
                 crates = crates + crate + ", ";
             }
-            crates = crates.substring(0, crates.length()-2);
+            crates = crates.substring(0, crates.length() - 2);
             cs.sendMessage(MessageUtil.PREFIX.getLocal() + MessageUtil.COMMAND_LIST.getLocal().replaceAll("%crates", crates));
             return true;
         }
-        
+
         return false;
     }
-    
-    
-    
+
 }
