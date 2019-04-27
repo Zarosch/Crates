4ff89da734073a4fb76391928661dcb611055906
Zarosch <kevin.velz@gmail.com>
2019-04-27 13:58:04 +0200

    :sparkles: Add spanish translation from zuhir :2

--- plugin/src/main/java/me/velz/crate/utils/MessageUtil.java
+++ plugin/src/main/java/me/velz/crate/utils/MessageUtil.java
@@ -5,30 +5,31 @@ import me.velz.crate.Crates;
 
 public enum MessageUtil {
 
-    PREFIX("", "§f[§eCrates§f] ", "§f[§eCrates§f] "),
-    ERROR_NOPERMISSIONS("", "§cYou dont have permissions.", "§cDazu hast du keine Berechtigung."),
-    ERROR_PLAYERNOTFOUND("", "§cThis player can not be found.", "§cDieser Spieler konnte nicht gefunden werden."),
-    ERROR_NONUMBER("", "§cPlease enter a valid number.", "§cBitte gebe eine gültige Zahl ein."),
-    ERROR_SYNTAX("", "§cSyntax error, please use: %command", "§cSyntax Fehler, bitte nutze: %command"),
-    ERROR_CRATENOTFOUND("", "§cThis crate can not be found.", "§cDieses Crate konnte nicht gefunden werden."),
-    ITEMWON("", "§aYou have §f§o%win §awon.", "§aDu hast §f§o%win §agewonnen."),
-    COMMAND_HELP_RELOAD("", "§6/crates reload §fReload Configuration", "§6/crates reload §fKonfiguration neuladen"),
-    COMMAND_HELP_GIVE("", "§6/crates give <Crate> [Player] §fGives you a crate", "§6/crates give <Crate> [Spieler] §fGebe dir ein Crate"),
-    COMMAND_HELP_LIST("", "§6/crates list §fList of all crates.", "§6/crates list §fListe aller Crates"),
-    COMMAND_RELOADED("", "§aConfiguration reloaded.", "§aKonfiguration neugeladen"),
-    COMMAND_GIVE("", "§aYou have give §f§o%player §athe crate §f§o%crate§a.", "§aDu hast §f§o%player §adie Crate §f§o%crate§a gegeben."),
-    COMMAND_LIST("", "§6All crates:§e %crates", "§6Alle verfügbaren Crates:§e %crates");
+    PREFIX("", "§f[§eCrates§f] ", "§f[§eCrates§f] ", "§f[§eCrates§f] "),
+    ERROR_NOPERMISSIONS("", "§cYou dont have permissions.", "§cDazu hast du keine Berechtigung.", "§cNo tienes permiso."),
+    ERROR_PLAYERNOTFOUND("", "§cThis player can not be found.", "§cDieser Spieler konnte nicht gefunden werden.", "§cJugador no encontrado"),
+    ERROR_NONUMBER("", "§cPlease enter a valid number.", "§cBitte gebe eine gültige Zahl ein.", "§cPor favor ingresa un número válido."),
+    ERROR_SYNTAX("", "§cSyntax error, please use: %command", "§cSyntax Fehler, bitte nutze: %command", "§cError de sintaxis, por favor use: %command"),
+    ERROR_CRATENOTFOUND("", "§cThis crate can not be found.", "§cDieses Crate konnte nicht gefunden werden.", "§cNo se pudo encontrar esa crate."),
+    ITEMWON("", "§aYou have §f§o%win §awon.", "§aDu hast §f§o%win §agewonnen.", "§aHas ganado: §f§o%win"),
+    COMMAND_HELP_RELOAD("", "§6/crates reload §fReload Configuration", "§6/crates reload §fKonfiguration neuladen", "§6/crates reload §fRecarga la configuración."),
+    COMMAND_HELP_GIVE("", "§6/crates give <Crate> [Player] §fGives you a crate", "§6/crates give <Crate> [Spieler] §fGebe dir ein Crate", "§6/crates give <crate> [jugador] §fDa una caja."),
+    COMMAND_HELP_LIST("", "§6/crates list §fList of all crates.", "§6/crates list §fListe aller Crates", "§6/crates list §fLista de crates disponibles."),
+    COMMAND_RELOADED("", "§aConfiguration reloaded.", "§aKonfiguration neugeladen", "§aConfiguración recargada."),
+    COMMAND_GIVE("", "§aYou have give §f§o%player §athe crate §f§o%crate§a.", "§aDu hast §f§o%player §adie Crate §f§o%crate§a gegeben.", "§aLe has dado una crate §f§o%crate§a a §f§o%player§a."),
+    COMMAND_LIST("", "§6All crates:§e %crates", "§6Alle verfügbaren Crates:§e %crates", "§6Crates disponibles:§e %crates");
 
     @Getter
     private String local;
 
     @Getter
-    private final String english, german;
+    private final String english, german, spanish;
 
-    private MessageUtil(String local, String english, String german) {
+    private MessageUtil(String local, String english, String german, String spanish) {
         this.local = local;
         this.english = english;
         this.german = german;
+        this.spanish = spanish;
     }
 
     public static void load() {
@@ -43,6 +44,12 @@ public enum MessageUtil {
             message_de.addDefault("message." + m.toString().replaceAll("_", ".").toLowerCase(), m.german.replaceAll("§", "&"));
         }
         message_de.save();
+
+        FileBuilder message_es = new FileBuilder("plugins/Crates", "messages_es.yml");
+        for (MessageUtil m : MessageUtil.values()) {
+            message_es.addDefault("message." + m.toString().replaceAll("_", ".").toLowerCase(), m.spanish.replaceAll("§", "&"));
+        }
+        message_es.save();
 
         FileBuilder message = new FileBuilder("plugins/Crates", "messages_" + Crates.getPlugin().getFileManager().getLanguage() + ".yml");
         if (!message.getFile().exists()) {
