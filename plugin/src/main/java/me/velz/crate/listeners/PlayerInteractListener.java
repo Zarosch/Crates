b16b4898dc06a0ed8674699abe3c73fe95d50c4a
Zarosch <kevin.velz@gmail.com>
2019-04-29 23:59:30 +0200

    :bug: Disable crate when no content is available

--- plugin/src/main/java/me/velz/crate/listeners/PlayerInteractListener.java
+++ plugin/src/main/java/me/velz/crate/listeners/PlayerInteractListener.java
@@ -28,27 +28,35 @@ public class PlayerInteractListener implements Listener {
     public void onPlayerInteract(PlayerInteractEvent event) {
         Player player = event.getPlayer();
         if (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) {
-            if(event.getItem() == null) return;
-            if(event.getItem().getItemMeta() == null) return;
-            if(event.getItem().getItemMeta().getDisplayName() == null) return;
+            if (event.getItem() == null) {
+                return;
+            }
+            if (event.getItem().getItemMeta() == null) {
+                return;
+            }
+            if (event.getItem().getItemMeta().getDisplayName() == null) {
+                return;
+            }
             for (Crate crate : plugin.getCrates().values()) {
                 if (event.getItem().getItemMeta().getDisplayName().equalsIgnoreCase(crate.getItem().getItemMeta().getDisplayName())) {
                     event.setCancelled(true);
                     if (plugin.inventoryContains(event.getPlayer().getInventory(), crate.getItem())) {
-                        if (plugin.getFileManager().isAnimation()) {
-                            Inventory inventory = Bukkit.createInventory(null, 27, crate.getName());
-                            inventory.setItem(4, new ItemBuilder().setDisplayName(" ").setMaterial(Material.HOPPER).build());
+                        if (!crate.getCrateItems().isEmpty()) {
+                            if (plugin.getFileManager().isAnimation()) {
+                                Inventory inventory = Bukkit.createInventory(null, 27, crate.getName());
+                                inventory.setItem(4, new ItemBuilder().setDisplayName(" ").setMaterial(Material.HOPPER).build());
 
-                            event.getPlayer().openInventory(inventory);
-                            new CrateOpening(plugin, event.getPlayer(), inventory, crate);
-                            plugin.removeFromInventory(event.getPlayer().getInventory(), crate.getItem());
-                        } else {
-                            Random random = new Random();
-                            CrateItem nextCrateItem = crate.getCrateItems().get(random.nextInt(crate.getCrateItems().size()));
-                            nextCrateItem.runCommands(player);
-                            player.playSound(player.getLocation(), plugin.getVersion().getSound("ENTITY_PLAYER_LEVELUP"), 10L, 10L);
-                            player.sendMessage(MessageUtil.PREFIX.getLocal() + MessageUtil.ITEMWON.getLocal().replaceAll("%win", nextCrateItem.getName()));
-                            plugin.removeFromInventory(event.getPlayer().getInventory(), crate.getItem());
+                                event.getPlayer().openInventory(inventory);
+                                new CrateOpening(plugin, event.getPlayer(), inventory, crate);
+                                plugin.removeFromInventory(event.getPlayer().getInventory(), crate.getItem());
+                            } else {
+                                Random random = new Random();
+                                CrateItem nextCrateItem = crate.getCrateItems().get(random.nextInt(crate.getCrateItems().size()));
+                                nextCrateItem.runCommands(player);
+                                player.playSound(player.getLocation(), plugin.getVersion().getSound("ENTITY_PLAYER_LEVELUP"), 10L, 10L);
+                                player.sendMessage(MessageUtil.PREFIX.getLocal() + MessageUtil.ITEMWON.getLocal().replaceAll("%win", nextCrateItem.getName()));
+                                plugin.removeFromInventory(event.getPlayer().getInventory(), crate.getItem());
+                            }
                         }
                     }
                 }
